ff28e856ab5279d0fbc910d83a0be7ea7bc9efb0
jesnie01 <jesperbnielsen@live.dk>
2023-10-24 09:29:32 +0200

    merged branches

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,11 +1,12 @@
+import java.util.Scanner;
 
 public class Calculator {
-
+Scanner in = new Scanner(System.in);
     /**
      * add two numbers
-     * @param no1
-     * @param no2
-     * @return
+     * @param no1 first number
+     * @param no2 second number
+     * @return no1 and no2 added together
      */
     public static int addTwoNumbers(int no1, int no2) {
         return no1 + no2;
